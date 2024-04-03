4fca73b45ff2d60c44182d769a00e87274c8ca19
Mosiur <mosiurzzaman99@gmail.com>
2024-04-03 08:58:38 +0600

    project created

---
+++ porject/src/test/java/com/web_application/porject/PorjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_application.porject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
